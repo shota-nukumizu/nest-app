75b62b8f89ec02c7ffac49afc79228015e7d8b0a
shota-nukumizu <author2000.1225@gmail.com>
2022-03-08 09:19:06 +0900

    create auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+
+export class AuthService {
+    login() {
+
+    }
+
+    signup() {
+        
+    }
+}
+
+const service = new AuthService()
